427186777933e5bc9b065acc0798aaf6930f4c3f
Mykhailo Dobryden <dobrydenmykhail@gmail.com>
2023-11-07 10:52:25 +0200

    Added views to create/edit blog post inside the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-p7)2qgfrq5nbklvqy$vausg*l7*cb8o6jt)ss$o-1)at1ye=yh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
